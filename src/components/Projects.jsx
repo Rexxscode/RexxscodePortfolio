1d8ad186365faa4bd16068b8c388d96eaa85b084
Hype AMD <adzkaalainufar2010rex@gmail.com>
2025-11-23 20:57:01 +0700

    fix: unify PROJECT folder case

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ import RexxCodePort5 from "../assets/PROJECT/14.jpg"
 import RexxyAdzka from "../assets/PROJECT/15.jpg"
 import Astrava from "../assets/PROJECT/16.jpg"
 import CakralogyITECHNO from "../assets/PROJECT/17.jpg"
-import GreenovaTech from "../assets/PROJECt/18.jpg"
+import GreenovaTech from "../assets/PROJECT/18.jpg"
 
 const projects = [
   {
